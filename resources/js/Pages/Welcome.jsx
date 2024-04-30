013d2c3a6d316ee157a2a27b2b7e64492acf6c6e
rraanggaaaa <dwiranggaoktaz@gmail.com>
2024-04-30 14:09:08 +0700

    alhamdulillah

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -2,6 +2,7 @@ import { Head } from '@inertiajs/react';
 import ReactLogo from '@/Components/ReactLogo';
 import DefaultStepper from '@/Components/DefaultStepper';
 import DemoAnimation from '@/Components/DemoAnimation';
+import TeamCard from '@/Components/TeamCard';
 import { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import {
@@ -25,46 +26,6 @@ const navigation = [
     { name: 'Insatalasi Breeze', href: '#instalasiBreeze' },
     { name: 'Demo', href: '#demo' },
 ];
-
-
-
-
-function TeamCard({ img, name, title }) {
-
-    return (
-        <Card className="rounded-lg bg-[#FAFAFA]" shadow={false}>
-            <CardBody className="text-center">
-                <Avatar
-                    src={img}
-                    alt={name}
-                    variant="circular"
-                    size="xxl"
-                    className="mx-auto mb-6 object-top w-20"
-                />
-                <Typography variant="h5" color="blue-gray" className="!font-medium text-lg">
-                    {name}
-                </Typography>
-                <Typography
-                    color="blue-gray"
-                    className="mb-2 !text-base !font-semibold text-gray-600"
-                >
-                    {title}
-                </Typography>
-                <div className="flex items-center justify-center gap-1.5">
-                    <IconButton variant="text" color="gray">
-                        <i className="fa-brands fa-twitter text-lg" />
-                    </IconButton>
-                    <IconButton variant="text" color="gray">
-                        <i className="fa-brands fa-linkedin text-lg" />
-                    </IconButton>
-                    <IconButton variant="text" color="gray">
-                        <i className="fa-brands fa-dribbble text-lg" />
-                    </IconButton>
-                </div>
-            </CardBody>
-        </Card>
-    );
-}
 
 const posts = [
     {
