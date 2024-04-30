906f421b010772dc42d3b883363c2c11c72fed53
rraanggaaaa <dwiranggaoktaz@gmail.com>
2024-04-30 07:01:02 +0700

    final

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -12,7 +12,11 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                        <div className="p-6 text-3xl text-gray-900 dark:text-gray-100">Kesimpulan</div>
+                    </div>
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-justify text-xl text-gray-900 dark:text-gray-100">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp; Laravel Breeze adalah paket autentikasi sederhana dan ringan untuk Laravel yang menyediakan implementasi dasar untuk fitur-fitur penting seperti login, registrasi, reset password, dan verifikasi email. Breeze menggunakan Blade dan Tailwind CSS untuk templating dan styling, menjadikannya pilihan ideal untuk pengembang yang menginginkan solusi autentikasi yang mudah diintegrasikan dan tidak membebani sistem. Proses instalasinya yang cepat dan sederhana memungkinkan pengembang untuk segera memulai dengan aplikasi Laravel yang dilengkapi sistem autentikasi, membuat Breeze menjadi pilihan yang praktis dan efisien bagi proyek-proyek baru.
+                        </div>
                     </div>
                 </div>
             </div>
